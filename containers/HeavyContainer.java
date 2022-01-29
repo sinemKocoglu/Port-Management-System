27a9c4534602fa2bcdf40880251a965f55117326
sinemKocoglu <98259272+sinemKocoglu@users.noreply.github.com>
2022-01-29 13:46:51 +0300

    Update HeavyContainer.java

--- containers/HeavyContainer.java
+++ containers/HeavyContainer.java
@@ -1,6 +1,3 @@
-
-//DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
-
 package containers;
 
 public class HeavyContainer extends Container{
@@ -41,8 +38,3 @@ public class HeavyContainer extends Container{
 	}
 	
 }
-
-
-
-//DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
-
